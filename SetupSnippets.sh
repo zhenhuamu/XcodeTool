d72c2b81b1919285958ff54644ad15fae737c013
muzh <muzh@2345.com>
2017-05-15 11:44:20 +0800

    shell脚本更新

--- SetupSnippets.sh
+++ SetupSnippets.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
-# 移动目录文件
-mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
 
-#建立文件软连接
+# 移动目录文件
+if [ -d ~/Library/Developer/Xcode/UserData/CodeSnippets ];then
+mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
+fi
+
+# 建立文件软连接
 
 SRC_HOME=`pwd`
 ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
